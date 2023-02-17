9000e300c6926a993f62380371b25b1546233de6
ROR2022 <rami.ror279@gmail.com>
2023-02-17 16:50:53 -0600

    o

--- src/UserConeccted.js
+++ src/UserConeccted.js
@@ -3,7 +3,7 @@ import { useProfile } from 'react-facebook';
 const UserConeccted = () => {
   const { profile, isLoading, error } = useProfile(['id', 'name']);
 
-  console.log('Profile:..',profile);
+  //console.log('Profile:..',profile);
   return (
     <div>
       UserConeccted
