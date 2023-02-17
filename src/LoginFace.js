8a8390e3dee33c734522049404274d4d61fd2102
ROR2022 <rami.ror279@gmail.com>
2023-02-17 14:38:31 -0600

    o

--- src/LoginFace.js
+++ src/LoginFace.js
@@ -1,17 +1,20 @@
-import { useLogin, useProfile } from 'react-facebook';
+import { useState } from 'react';
+import { useLogin } from 'react-facebook';
+import UserConeccted from './UserConeccted';
 
 const LoginFace = () => {
+  const [isConnected, setIsConnected] = useState(false);
   const { login, isLoading, status, error} = useLogin();
-  const { profile } = useProfile(['id', 'name']);
+  //const { profile } = useProfile(['id', 'name']);
 
   async function handleLogin() {
     try {
       const response = await login({
         scope: 'email',
       });
-
+      setIsConnected(response.status);
       console.log('response:..',response);
-      console.log('profile:..',profile);
+      //console.log('profile:..',profile);
     } catch (error) {
       console.log(error.message);
     }
@@ -23,6 +26,7 @@ const LoginFace = () => {
       <button onClick={handleLogin} disabled={isLoading}>
       Login via Facebook
     </button>
+    <UserConeccted/>
       </div>
   )
 }
