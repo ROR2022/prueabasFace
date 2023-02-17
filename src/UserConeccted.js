3984f0cb9849b19f12cef0e69c49216377d9a4bf
ROR2022 <rami.ror279@gmail.com>
2023-02-17 15:08:07 -0600

    o

--- src/UserConeccted.js
+++ src/UserConeccted.js
@@ -1,8 +1,13 @@
-import React from 'react'
+import { useProfile } from 'react-facebook';
 
 const UserConeccted = () => {
+  const { profile, isLoading, error } = useProfile(['id', 'name']);
+
   return (
-    <div>UserConeccted</div>
+    <div>
+      UserConeccted
+      <h3>{profile?.name} has id: {profile?.id}</h3>
+      </div>
   )
 }
 
