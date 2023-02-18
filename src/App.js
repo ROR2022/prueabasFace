fde8323af3765b6d7e8b5a2ff7ac118ab13d6fa3
ROR2022 <rami.ror279@gmail.com>
2023-02-18 12:58:05 -0600

    o

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import LoginFace from "./LoginFace";
-//import { FacebookProvider} from 'react-facebook';
-//<FacebookProvider appId="933422631180070"></FacebookProvider> 
+import { FacebookProvider} from 'react-facebook';
+ 
 
 function App() {
   
   return (
-    
+<FacebookProvider appId="933422631180070">
     <div>
       <header>
        <h1>Pruebas con Face</h1>
        <LoginFace/>
       </header>
     </div>
-    
+    </FacebookProvider>    
   );
 }
 
