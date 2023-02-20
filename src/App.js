be157f1be1add61c10b454c798caacc9f17ed08c
ROR2022 <rami.ror279@gmail.com>
2023-02-20 12:40:27 -0600

    pruebas

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import LoginFace from "./LoginFace";
 import { FacebookProvider} from 'react-facebook';
  
-
+//886374405903867  QA
+//933422631180070  PRD
 function App() {
   
   return (
-<FacebookProvider appId="933422631180070">
+<FacebookProvider appId="886374405903867">
     <div>
       <header>
        <h1>Pruebas con Face</h1>
